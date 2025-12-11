b857360cab5c9ae49fbcf936d715c28734e67b1f
KCode-YVR <khemin.pr@gmail.com>
2025-12-11 01:03:22 -0800

    Changed popup.js to output the current url that the extension is used on. This is used to show that the url of the current tab can be stored to be used by backend.

--- main/popup.js
+++ main/popup.js
@@ -1,1 +1,20 @@
-console.log("Nice to meet you")
+const scanButton = document.getElementById('scanButton');
+const resultsDiv = document.getElementById('results');
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+async function handleScanClick() {
+    resultsDiv.innerHTML = "You pressed the button. Getting URLs...";
+    const curTab = await getCurrentTab();
+    const tabURL = curTab.url;
+    resultsDiv.innerHTML += 'Current Tab URL is: ' + tabURL;
+    console.log("Current URL:", tabURL);
+}
+
+scanButton.addEventListener('click', handleScanClick);
+
